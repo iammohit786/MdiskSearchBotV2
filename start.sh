66e02615671673fea7fee6900dc85765017c5836
iammohit786 <109838910+iammohit786@users.noreply.github.com>
2022-12-30 00:05:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Cyniteofficial/MdiskSearchBotV2.git /MdiskSearchBotV2
-cd /MdiskSearchBotV2
+git clone https://github.com/contact_todayfm/TODAYFM_LINK_SEARCH_BOT.git /TODAYFM_LINK_SEARCH_BOT
+cd /TODAYFM_LINK_SEARCH_BOT
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
